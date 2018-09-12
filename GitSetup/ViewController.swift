e1344baf426f72f5da7e1208b1a011a7c59d9a29
Daniel Rabe <danielrabe@ravenzclaw.local>
2018-09-12 13:26:09 -0700

    I added label to my story board.  Yup!

--- GitSetup/ViewController.swift
+++ GitSetup/ViewController.swift
@@ -12,10 +12,14 @@ class ViewController: UIViewController {
     
     let message = "Hello Gitamba!!"
 
+    @IBOutlet weak var reverseLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
-        print(reverse(text: "stressed"))
+        
+        let reversed = reverse(text: "stressed!")
+        print(reversed)
+        reverseLabel.text = reversed
     }
     func reverse(text: String) -> String {
         return String(text.reversed())
